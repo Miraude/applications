cff61d6861bce0a7c7eb045f85bbf4b5e13a0fb9
Johnny Giang <Johnny.Giang@hotmail.com>
2022-10-27 14:44:01 +0200

    Miscellaneous

--- SuggestionApp/SuggestionAppUI/obj/Debug/net6.0/SuggestionAppUI.AssemblyInfo.cs
+++ SuggestionApp/SuggestionAppUI/obj/Debug/net6.0/SuggestionAppUI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("fcf1e1c1-28c3-4b56-811a-1c506d93ab36")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuggestionAppUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
