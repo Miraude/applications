1433696202ce48871c270a8bee5037b6e2095e4f
Johnny Giang <Johnny.Giang@hotmail.com>
2022-10-30 19:40:25 +0100

    Added helper for authState to reduce repetitive code

---
+++ SuggestionApp/SuggestionAppUI/Helpers/AuthenticationStateProviderHelpers.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SuggestionAppUI.Helpers;
+
+public static class AuthenticationStateProviderHelpers
+{
+   public static async Task<User> GetUserFromAuth(
+      this AuthenticationStateProvider provider, 
+      IUser user)
+   {
+      var authState = await provider.GetAuthenticationStateAsync();
+      string objectId = authState.User.Claims.FirstOrDefault(c => c.Type.Contains("objectidentifier"))?.Value;
+
+      return await user.GetUserFromAuthentication(objectId);
+   }
+}
