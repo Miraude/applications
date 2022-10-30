3692f358f73949e815f89f4de78cc00716c0e9c5
Johnny Giang <Johnny.Giang@hotmail.com>
2022-10-30 19:39:57 +0100

    Updated global usings

--- SuggestionApp/SuggestionAppUI/GlobalUsings.cs
+++ SuggestionApp/SuggestionAppUI/GlobalUsings.cs
@@ -1,2 +1,6 @@
 ﻿global using SuggestionAppLibrary.Models;
 global using SuggestionAppLibrary.DataAccess;
+global using System.Threading.Tasks;
+global using System.Linq;
+global using SuggestionAppUI.Helpers;
+global using Microsoft.AspNetCore.Components.Authorization;
