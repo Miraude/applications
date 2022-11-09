cecf0f782e8ef9507c9c359f320537a6ac8a33fe
Johnny Giang <Johnny.Giang@hotmail.com>
2022-11-09 15:31:58 +0100

    Completed test

---
+++ SokigoTestComplete/SokigoTest/Controllers/PersonController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SokigoTest.Models;
+
+namespace SokigoTest.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly  AddressAppContext _context;
+
+        public PersonController(AddressAppContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<PersonsModel> objList = _context.Persons;
+
+            return View(objList);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(PersonsModel obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                _context.Persons.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+    }
+}
---
+++ SokigoTestComplete/SokigoTest/DataInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using System.Data;
+using Microsoft.IdentityModel.Tokens;
+using SokigoTest.Models;
+
+namespace SokigoTest
+{
+    public class DataInitializer
+    {
+           private readonly AddressAppContext _dbContext;
+
+        public DataInitializer(AddressAppContext dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+    }
+}
